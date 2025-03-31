e060d18d58a44e59cc29dfaac9e6b78c0c404eaf
Carolina <carolina.peseca@gmail.com>
2025-03-31 11:35:51 +0000

    first commit

--- app.js
+++ app.js
@@ -117,4 +117,19 @@ document.getElementById("threeColumnView").addEventListener("click", function() 
     buttonOne.style.color = "#AC8EFA";
   });
   
+  document.addEventListener("DOMContentLoaded", function () {
+    let form = document.querySelector('.needs-validation');
+
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            event.preventDefault();
+            alert("Formulario enviado correctamente!");
+        }
+
+        form.classList.add('was-validated');
+    });
+});
     
